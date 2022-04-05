746f07b3429ef605457b6e4e908dc042111cf1ff
unknown <nsaedae@gmail.com>
2022-04-05 12:15:34 +0900

    2022/04/05 수업예제

--- Ch05/src/main/java/kr/co/ch05/controller/MainController.java
+++ Ch05/src/main/java/kr/co/ch05/controller/MainController.java
@@ -1,5 +1,8 @@
 package kr.co.ch05.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -15,25 +18,42 @@ public class MainController {
 	public String index(HttpServletRequest req, Model model) {
 		
 		String title = "Spring Thymeleaf 실습";
-		String hello = "Hello Thymeleaf";
+		String hello = "Hello Thymeleaf!";
 		
-		UserVo user = new UserVo();
-		user.setUid("a101");
-		user.setName("홍길동");
-		user.setHp("010-1234-1111");
-		user.setAge(21);
+		UserVo user1 = new UserVo();
+		user1.setUid("a100");
+		user1.setName("홍길동");
+		user1.setHp("010-1234-1111");
+		user1.setAge(19);
+		
+		UserVo user2 = null;
+		
+		List<UserVo> users = new ArrayList<>();
+		users.add(new UserVo("a101", "김유신", "010-1234-1001", 23));
+		users.add(new UserVo("a102", "김춘추", "010-1234-1002", 21));
+		users.add(new UserVo("a103", "장보고", "010-1234-1003", 31));
+		users.add(new UserVo("a104", "강감찬", "010-1234-1004", 42));
+		users.add(new UserVo("a105", "이순신", "010-1234-1005", 50));
+		
 		
 		// Spring에서는 컴포넌트간 데이터 공유를 request 사용 권장하지 않음 
 		req.setAttribute("title", title);
 		
 		// Spring에서는 컴포넌트간 데이터 공유를 Model객체 사용 권장
 		model.addAttribute("hello", hello);
-		model.addAttribute(user);
-		
-		
+		model.addAttribute("user1", user1);
+		model.addAttribute("user2", user2);
+		model.addAttribute("users", users);
 		
 		return "/index";
 	}
+	
+	
+	@GetMapping("/include")
+	public String include() {
+		return "/include";
+	}
+	
 	
 	
 }

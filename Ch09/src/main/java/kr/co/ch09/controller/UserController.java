48a7f2a1043173666dd50cf6224d05c123faaa96
unknown <nsaedae@gmail.com>
2022-04-13 11:12:11 +0900

    2022/04/13 수업예제

--- Ch09/src/main/java/kr/co/ch09/controller/UserController.java
+++ Ch09/src/main/java/kr/co/ch09/controller/UserController.java
@@ -4,7 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.ch09.service.UserService;
@@ -21,6 +27,28 @@ public class UserController {
 	public List<UserVo> list() {
 		List<UserVo> users = service.selectUsers();
 		return users;
+	}
+	
+	@ResponseBody
+	@PostMapping("/user")
+	public List<UserVo> register(UserVo vo) {
+		service.insertUser(vo);
+		return service.selectUsers();
+	}
+	
+	@PutMapping("/user/{uid}")
+	public List<UserVo> modify(@PathVariable("uid") String uid, UserVo vo) {
+		
+		vo.setUid(uid);
+		
+		service.updateUser(vo);
+		return service.selectUsers();
+	}
+	
+	@DeleteMapping("/user/{uid}")
+	public List<UserVo> delete(@PathVariable("uid") String uid) {
+		service.deleteUser(uid);
+		return service.selectUsers();
 	}
 	
 	

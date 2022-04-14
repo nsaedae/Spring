58a02516bc070142547745176729e94827761e9e
unknown <nsaedae@gmail.com>
2022-04-14 11:19:12 +0900

    2022/04/14 수업예제

---
+++ Ch08/src/main/java/kr/co/ch08/controller/User3Controller.java
@@ -0,0 +1,73 @@
+package kr.co.ch08.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import kr.co.ch08.service.User2Service;
+import kr.co.ch08.vo.User2Vo;
+
+
+@SessionAttributes("sessUser")
+@Controller
+public class User3Controller {
+
+	@Autowired
+	private User2Service service;
+	
+	
+	@GetMapping("/user3/login")
+	public String login() {
+		return "/user3/login";
+	}
+	
+	@PostMapping("/user3/login")
+	public String login(User2Vo vo, Model model) {
+		
+		User2Vo user = service.selectUser(vo);
+		
+		if(user == null) {
+			return "redirect:/user3/login?success=100";
+		}else {
+			model.addAttribute("sessUser", user);
+			return "redirect:/user3/loginSuccess";
+		}
+	}
+	
+	
+	@GetMapping("/user3/logout")
+	public String logout(SessionStatus status) {
+		// 세션해제
+		status.setComplete();
+		
+		return "redirect:/user3/login?success=103";
+	}
+	
+		
+	@GetMapping("/user3/register")
+	public String register() {
+		return "/user3/register";
+	}
+	
+	@PostMapping("/user3/register")
+	public String register(User2Vo vo) {
+		service.insertUser(vo);
+		return "redirect:/user3/login";
+	}
+	
+	@GetMapping("/user3/loginSuccess")
+	public String loginSuccess(@ModelAttribute("sessUser")User2Vo sessUser) {
+		
+		if(sessUser == null) {
+			return "redirect:/user3/login?success=101";
+		}else {
+			return "/user3/loginSuccess";
+		}
+	}
+	
+}
--- Ch08/src/main/java/kr/co/ch08/dao/User2Dao.java
+++ Ch08/src/main/java/kr/co/ch08/dao/User2Dao.java
@@ -12,7 +12,10 @@ import kr.co.ch08.vo.User2Vo;
 public interface User2Dao {
 
 	public void insertUser(User2Vo vo);
+	// 로그인용
 	public User2Vo selectUser(User2Vo vo);
+	// 조회용
+	public User2Vo selectUser(String uid);
 	public List<User2Vo> selectUsers();
 	public void updateUser(User2Vo vo);
 	public void deleteUser(String uid);

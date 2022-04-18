d278150e9e0442d8aa0f76d3c7668ec9b4a2f2ac
unknown <nsaedae@gmail.com>
2022-04-18 10:06:28 +0900

    2022/04/18 Sboard1 수업예제

---
+++ Sboard1/src/main/java/kr/co/sboard1/controller/MainController.java
@@ -0,0 +1,19 @@
+package kr.co.sboard1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	
+	
+	@GetMapping(value = {"", "/index"})
+	public String index() {
+		
+		
+		return "/index";
+	}
+	
+	
+}
---
+++ Sboard1/src/test/java/kr/co/sboard1/Sboard1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.sboard1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sboard1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

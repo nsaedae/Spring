11be07433b282114d62ea797f6471d81ac72464e
unknown <nsaedae@gmail.com>
2022-04-25 12:07:03 +0900

    2022/04/25 수업예제

--- Farmstory/src/main/java/kr/co/farmstory/controller/BoardController.java
+++ Farmstory/src/main/java/kr/co/farmstory/controller/BoardController.java
@@ -1,13 +1,20 @@
 package kr.co.farmstory.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
 	@GetMapping("/board/list")
-	public String list() {
+	public String list(Model model, String cate, String type) {
+		
+		
+		
+		model.addAttribute("cate", cate);
+		model.addAttribute("type", type);
+		
 		return "/board/list";
 	}
 	

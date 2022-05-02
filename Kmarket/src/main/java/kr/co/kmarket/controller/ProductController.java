3fdfcafbca33549a1e4e9f2d381c4a0b79d0e140
unknown <nsaedae@gmail.com>
2022-05-02 11:18:12 +0900

    2022/05/02 수업예제

--- Kmarket/src/main/java/kr/co/kmarket/controller/ProductController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/ProductController.java
@@ -1,5 +1,34 @@
 package kr.co.kmarket.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductController {
 
+	@GetMapping("/product/cart")
+	public String cart() {
+		return "/product/cart";
+	}
+	@GetMapping("/product/complete")
+	public String complete() {
+		return "/product/complete";
+	}
+	@GetMapping("/product/list")
+	public String list() {
+		return "/product/list";
+	}
+	@GetMapping("/product/order")
+	public String order() {
+		return "/product/order";
+	}
+	@GetMapping("/product/search")
+	public String search() {
+		return "/product/search";
+	}
+	@GetMapping("/product/view")
+	public String view() {
+		return "/product/view";
+	}
+	
 }

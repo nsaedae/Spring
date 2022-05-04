61ce49547b1622c6ee6e460628b00ced397ff576
unknown <nsaedae@gmail.com>
2022-05-04 10:32:59 +0900

    2022/05/04 수업예제

--- Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
@@ -1,15 +1,32 @@
 package kr.co.kmarket.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.co.kmarket.service.MainService;
+import kr.co.kmarket.vo.CategoriesVo;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	private MainService service;
+	
 	
 	@GetMapping(value = {"/", "/index"})
 	public String index() {
 		return "/index";
+	}
+	
+	
+	@ResponseBody
+	@GetMapping("/getCategories")
+	public List<CategoriesVo> getCategories() {
+		return service.selectCategories();
 	}
 	
 }
--- Kmarket/src/main/java/kr/co/kmarket/dao/MainDao.java
+++ Kmarket/src/main/java/kr/co/kmarket/dao/MainDao.java
@@ -1,11 +1,17 @@
 package kr.co.kmarket.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import kr.co.kmarket.vo.CategoriesVo;
 
 @Mapper
 @Repository
 public interface MainDao {
+	
+	public List<CategoriesVo> selectCategories();
 
 	
 }
